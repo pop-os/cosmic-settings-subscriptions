29b8d3feba2832d1ad9c2c6972c86047df07feaa
Ian Douglas Scott <idscott@system76.com>
2024-06-13 18:12:19 -0700

    kbdbacklight: Listen for upower signal of changed brightness
    Both the applet and OSD will want to use this to show updates when the
    brightness changes.
    
    There is no need to disable listening for this signal when the applet is
    closed. We don't expect this signal to happen too frequently.
    
    `Get` can still be used when showing the applet, as a fallback if there
    are some backlights that don't generate this signal.

--- src/upower/kbdbacklight.rs
+++ src/upower/kbdbacklight.rs
@@ -1,20 +1,21 @@
 // Copyright 2024 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use futures::SinkExt;
+use futures::{SinkExt, StreamExt};
 use std::{fmt::Debug, hash::Hash};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
-use upower_dbus::KbdBacklightProxy;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use upower_dbus::{BrightnessChanged, KbdBacklightProxy};
 
 pub fn kbd_backlight_subscription<I: 'static + Hash + Copy + Send + Sync + Debug>(
     id: I,
 ) -> iced_futures::Subscription<KeyboardBacklightUpdate> {
     iced_futures::subscription::channel(id, 50, move |mut output| async move {
-        let mut state = State::Ready;
-
-        loop {
-            state = start_listening(state, &mut output).await;
+        if let Err(err) = listen(&mut output).await {
+            log::error!("Error listening to KbdBacklight: {}", err);
         }
+
+        futures::future::pending().await
     })
 }
 
@@ -28,58 +29,61 @@ pub enum State {
     Finished,
 }
 
+enum Event {
+    BrightnessChanged(BrightnessChanged),
+    Request(KeyboardBacklightRequest),
+}
+
 async fn get_brightness(kbd_proxy: &KbdBacklightProxy<'_>) -> zbus::Result<f64> {
     Ok(kbd_proxy.get_brightness().await? as f64 / kbd_proxy.get_max_brightness().await? as f64)
 }
 
-async fn start_listening(
-    state: State,
+async fn listen(
     output: &mut futures::channel::mpsc::Sender<KeyboardBacklightUpdate>,
-) -> State {
-    match state {
-        State::Ready => {
-            let conn = match zbus::Connection::system().await {
-                Ok(conn) => conn,
-                Err(_) => return State::Finished,
-            };
-            let kbd_proxy = match KbdBacklightProxy::builder(&conn).build().await {
-                Ok(p) => p,
-                Err(_) => return State::Finished,
-            };
-            let (tx, rx) = unbounded_channel();
+) -> zbus::Result<()> {
+    let conn = zbus::Connection::system().await?;
+    let kbd_proxy = KbdBacklightProxy::builder(&conn).build().await?;
+    let (tx, rx) = unbounded_channel();
 
-            let b = get_brightness(&kbd_proxy).await.ok();
-            _ = output.send(KeyboardBacklightUpdate::Sender(tx)).await;
-            _ = output.send(KeyboardBacklightUpdate::Brightness(b)).await;
+    let b = get_brightness(&kbd_proxy).await.ok();
+    _ = output.send(KeyboardBacklightUpdate::Sender(tx)).await;
+    _ = output.send(KeyboardBacklightUpdate::Brightness(b)).await;
 
-            State::Waiting(kbd_proxy, rx)
-        }
-        State::Waiting(proxy, mut rx) => match rx.recv().await {
-            Some(req) => match req {
+    let brightness_changed_stream = kbd_proxy.receive_brightness_changed().await?;
+
+    let mut stream = futures::stream::select(
+        UnboundedReceiverStream::new(rx).map(Event::Request),
+        brightness_changed_stream.map(Event::BrightnessChanged),
+    );
+    while let Some(event) = stream.next().await {
+        match event {
+            Event::BrightnessChanged(_changed) => {
+                let b = get_brightness(&kbd_proxy).await.ok();
+                _ = output.send(KeyboardBacklightUpdate::Brightness(b)).await;
+            }
+            Event::Request(req) => match req {
                 KeyboardBacklightRequest::Get => {
-                    let b = get_brightness(&proxy).await.ok();
+                    let b = get_brightness(&kbd_proxy).await.ok();
                     _ = output.send(KeyboardBacklightUpdate::Brightness(b)).await;
-                    State::Waiting(proxy, rx)
                 }
                 KeyboardBacklightRequest::Set(value) => {
-                    if let Ok(max_brightness) = proxy.get_max_brightness().await {
+                    if let Ok(max_brightness) = kbd_proxy.get_max_brightness().await {
                         let value = value.clamp(0., 1.) * (max_brightness as f64);
                         let value = value.round() as i32;
-                        let _ = proxy.set_brightness(value).await;
+                        let _ = kbd_proxy.set_brightness(value).await;
                     }
-
-                    State::Waiting(proxy, rx)
                 }
             },
-            None => State::Finished,
-        },
-        State::Finished => futures::future::pending().await,
+        }
     }
+
+    Ok(())
 }
 
 #[derive(Debug, Clone)]
 pub enum KeyboardBacklightUpdate {
     Sender(UnboundedSender<KeyboardBacklightRequest>),
+    // TODO: Send brightness and max brightness as integers
     Brightness(Option<f64>),
 }
 
