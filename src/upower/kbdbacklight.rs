21cc79434fa24f1fbd6dc4e8d394aa51d5ee12d9
Ian Douglas Scott <idscott@system76.com>
2024-07-01 13:24:20 -0700

    kbdbacklight: Expose brightness and max as i32
    This provides more flexibility to the client than a single f64.

--- src/upower/kbdbacklight.rs
+++ src/upower/kbdbacklight.rs
@@ -1,6 +1,9 @@
 // Copyright 2024 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
+// TODO Test how this handles upower starting after applet does, if it ever is
+// started or restarted.
+
 use futures::{FutureExt, Stream, StreamExt};
 use std::{fmt::Debug, hash::Hash};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
@@ -30,22 +33,20 @@ enum Event {
     Request(KeyboardBacklightRequest),
 }
 
-async fn get_brightness(kbd_proxy: &KbdBacklightProxy<'_>) -> zbus::Result<f64> {
-    Ok(kbd_proxy.get_brightness().await? as f64 / kbd_proxy.get_max_brightness().await? as f64)
-}
-
 async fn events() -> zbus::Result<impl Stream<Item = KeyboardBacklightUpdate>> {
     let conn = zbus::Connection::system().await?;
     let kbd_proxy = KbdBacklightProxy::builder(&conn).build().await?;
     let (tx, rx) = unbounded_channel();
 
-    let b = get_brightness(&kbd_proxy).await.ok();
+    let max_brightness = kbd_proxy.get_max_brightness().await?;
+    let brightness = kbd_proxy.get_brightness().await?;
 
     let brightness_changed_stream = kbd_proxy.receive_brightness_changed().await?;
 
     let initial = futures::stream::iter([
         KeyboardBacklightUpdate::Sender(tx),
-        KeyboardBacklightUpdate::Brightness(b),
+        KeyboardBacklightUpdate::MaxBrightness(max_brightness),
+        KeyboardBacklightUpdate::Brightness(brightness),
     ]);
     let stream = futures::stream::select(
         UnboundedReceiverStream::new(rx).map(Event::Request),
@@ -55,21 +56,23 @@ async fn events() -> zbus::Result<impl Stream<Item = KeyboardBacklightUpdate>> {
         let kbd_proxy = kbd_proxy.clone();
         async move {
             match event {
-                Event::BrightnessChanged(_changed) => {
-                    let b = get_brightness(&kbd_proxy).await.ok();
-                    Some(KeyboardBacklightUpdate::Brightness(b))
+                Event::BrightnessChanged(changed) => {
+                    if let Ok(args) = changed.args() {
+                        Some(KeyboardBacklightUpdate::Brightness(*args.value()))
+                    } else {
+                        None
+                    }
                 }
                 Event::Request(req) => match req {
                     KeyboardBacklightRequest::Get => {
-                        let b = get_brightness(&kbd_proxy).await.ok();
-                        Some(KeyboardBacklightUpdate::Brightness(b))
+                        if let Ok(brightness) = kbd_proxy.get_brightness().await {
+                            Some(KeyboardBacklightUpdate::Brightness(brightness))
+                        } else {
+                            None
+                        }
                     }
                     KeyboardBacklightRequest::Set(value) => {
-                        if let Ok(max_brightness) = kbd_proxy.get_max_brightness().await {
-                            let value = value.clamp(0., 1.) * (max_brightness as f64);
-                            let value = value.round() as i32;
-                            let _ = kbd_proxy.set_brightness(value).await;
-                        }
+                        let _ = kbd_proxy.set_brightness(value).await;
                         None
                     }
                 },
@@ -81,12 +84,12 @@ async fn events() -> zbus::Result<impl Stream<Item = KeyboardBacklightUpdate>> {
 #[derive(Debug, Clone)]
 pub enum KeyboardBacklightUpdate {
     Sender(UnboundedSender<KeyboardBacklightRequest>),
-    // TODO: Send brightness and max brightness as integers
-    Brightness(Option<f64>),
+    Brightness(i32),
+    MaxBrightness(i32),
 }
 
 #[derive(Debug, Clone)]
 pub enum KeyboardBacklightRequest {
     Get,
-    Set(f64),
+    Set(i32),
 }
