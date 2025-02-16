66307519e07902c826a69b981d71c6b5f730c910
Alexander Bachmann <a.t.bachmann@gmail.com>
2025-02-16 23:22:22 +0100

    refactor(bluetooth): move subscription code out of cosmic-settings

--- src/bluetooth/mod.rs
+++ src/bluetooth/mod.rs
@@ -6,17 +6,25 @@ use zbus::zvariant::OwnedObjectPath;
 
 mod adapter;
 mod device;
+pub mod subscription;
 
 pub use adapter::*;
 pub use device::*;
 
 #[derive(Clone, Debug)]
 pub enum Event {
+    AddedAdapter(OwnedObjectPath, Adapter),
+    AddedDevice(OwnedObjectPath, Device),
     DBusError(String),
+    DBusServiceUnknown,
     DeviceFailed(OwnedObjectPath),
     Ok,
+    RemovedAdapter(OwnedObjectPath),
+    RemovedDevice(OwnedObjectPath),
     SetAdapters(HashMap<OwnedObjectPath, Adapter>),
     SetDevices(HashMap<OwnedObjectPath, Device>),
+    UpdatedAdapter(OwnedObjectPath, Vec<AdapterUpdate>),
+    UpdatedDevice(OwnedObjectPath, Vec<DeviceUpdate>),
 }
 
 #[derive(Default, Debug, Clone, Copy, Eq, PartialEq)]
